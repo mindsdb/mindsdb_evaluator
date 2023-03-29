d226fa595608fe4c778ca2ad731acbd11c8eb3a3
vijay-jaisankar <vijayjaisankar.vj@gmail.com>
2023-03-29 16:22:11 +0530

    bugfix: flake8 warnings

--- mindsdb_evaluator/accuracy/classification.py
+++ mindsdb_evaluator/accuracy/classification.py
@@ -10,6 +10,7 @@ def evaluate_multilabel_accuracy(y_true, y_pred, **kwargs):
     pred_values = y_pred['prediction']
     return f1_score(y_true, pred_values, average='weighted')
 
+
 def evaluate_top_k_accuracy(y_true, y_pred, **kwargs):
     """
     Evaluates the number of times where the correct label is among the top k labels
@@ -18,4 +19,4 @@ def evaluate_top_k_accuracy(y_true, y_pred, **kwargs):
     """
     pred_values = y_pred['prediction']
     k = kwargs['k'] if 'k' in kwargs else 2
-    return top_k_accuracy_score(y_true, pred_values, k = k)
+    return top_k_accuracy_score(y_true, pred_values, k=k)

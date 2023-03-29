770dde0b063807ec3c87269652ec1496c9b05c3b
vijay-jaisankar <vijayjaisankar.vj@gmail.com>
2023-03-29 16:20:20 +0530

    feat: expose top-k accuracy metric from sklearn

--- mindsdb_evaluator/accuracy/classification.py
+++ mindsdb_evaluator/accuracy/classification.py
@@ -1,4 +1,4 @@
-from sklearn.metrics import f1_score
+from sklearn.metrics import f1_score, top_k_accuracy_score
 
 
 def evaluate_multilabel_accuracy(y_true, y_pred, **kwargs):
@@ -9,3 +9,13 @@ def evaluate_multilabel_accuracy(y_true, y_pred, **kwargs):
     """
     pred_values = y_pred['prediction']
     return f1_score(y_true, pred_values, average='weighted')
+
+def evaluate_top_k_accuracy(y_true, y_pred, **kwargs):
+    """
+    Evaluates the number of times where the correct label is among the top k labels
+
+    :return: top k accuracy score of y_pred and ground truths.
+    """
+    pred_values = y_pred['prediction']
+    k = kwargs['k'] if 'k' in kwargs else 2
+    return top_k_accuracy_score(y_true, pred_values, k = k)

a9b7358935582ef447da5e82a9224902afff1915
vijay-jaisankar <vijayjaisankar.vj@gmail.com>
2023-03-30 04:06:13 +0530

    test: add base sklearn failsafe checks ref: #16

--- tests/test_classification_accs.py
+++ tests/test_classification_accs.py
@@ -1,6 +1,7 @@
 import unittest
 import numpy as np
 from mindsdb_evaluator.accuracy.classification import evaluate_multilabel_accuracy, evaluate_top_k_accuracy
+from sklearn.metrics import top_k_accuracy_score
 
 
 class TestClassificationAccuracies(unittest.TestCase):
@@ -32,7 +33,10 @@ class TestClassificationAccuracies(unittest.TestCase):
         # vacuous case: no error
         true = np.array([1, 1, 1, 0, 0, 0])
         y_pred_true = {'prediction': true}
-        self.assertEqual(evaluate_top_k_accuracy(true, y_pred_true, k=1), 1.0)
+        ground_accuracy = top_k_accuracy_score(y_true=true, y_score=true, k=1, normalize=True)
+        module_accuracy = evaluate_top_k_accuracy(true, y_pred_true, k=1)
+        self.assertEqual(module_accuracy, 1.0)
+        self.assertEqual(module_accuracy, ground_accuracy)
 
         # random
         y_true = np.array([0, 1, 2, 2])
@@ -41,7 +45,10 @@ class TestClassificationAccuracies(unittest.TestCase):
                         [0.2, 0.4, 0.3],
                         [0.7, 0.2, 0.1]])
         y_pred = {'prediction': pred}
-        self.assertAlmostEqual(evaluate_top_k_accuracy(y_true, y_pred), 0.75)
+        ground_accuracy = top_k_accuracy_score(y_true=y_true, y_score=pred, k=2, normalize=True)
+        module_accuracy = evaluate_top_k_accuracy(y_true, y_pred)
+        self.assertAlmostEqual(module_accuracy, 0.75, 10)
+        self.assertEqual(module_accuracy, ground_accuracy)
 
         # large error
         y_true = np.array([0, 1, 2, 3])
@@ -50,7 +57,10 @@ class TestClassificationAccuracies(unittest.TestCase):
                         [0.3, 0.3, 0.1, 0.3],
                         [0.3, 0.3, 0.3, 0.1]])
         y_pred = {'prediction': pred}
-        self.assertAlmostEqual(evaluate_top_k_accuracy(y_true, y_pred, k=3), 0.25)
+        ground_accuracy = top_k_accuracy_score(y_true=y_true, y_score=pred, k=3, normalize=True)
+        module_accuracy = evaluate_top_k_accuracy(y_true, y_pred, k=3)
+        self.assertAlmostEqual(module_accuracy, 0.25, 10)
+        self.assertEqual(module_accuracy, ground_accuracy)
 
         # small error
         y_true = np.array([0, 1, 2, 3, 3])
@@ -60,5 +70,7 @@ class TestClassificationAccuracies(unittest.TestCase):
                         [0.3, 0.3, 0.3, 0.1],
                         [0.1, 0.3, 0.3, 0.3]])
         y_pred = {'prediction': pred}
-        self.assertAlmostEqual(evaluate_top_k_accuracy(y_true, y_pred, k=3), 0.80)
-
+        ground_accuracy = top_k_accuracy_score(y_true=y_true, y_score=pred, k=3, normalize=True)
+        module_accuracy = evaluate_top_k_accuracy(y_true, y_pred, k=3)
+        self.assertAlmostEqual(module_accuracy, 0.8, 10)
+        self.assertEqual(module_accuracy, ground_accuracy)

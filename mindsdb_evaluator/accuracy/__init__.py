ab94d1b52e97d9917c4e0539be936bf34ee9b086
vijay-jaisankar <vijayjaisankar.vj@gmail.com>
2023-03-29 16:51:26 +0530

    test: add tests for top-k-accuracy, chore: update package spec

--- mindsdb_evaluator/accuracy/__init__.py
+++ mindsdb_evaluator/accuracy/__init__.py
@@ -1,6 +1,6 @@
 from mindsdb_evaluator.accuracy.general import evaluate_accuracy, evaluate_accuracies
 from mindsdb_evaluator.accuracy.regression import evaluate_regression_accuracy
-from mindsdb_evaluator.accuracy.classification import evaluate_multilabel_accuracy
+from mindsdb_evaluator.accuracy.classification import evaluate_multilabel_accuracy, evaluate_top_k_accuracy
 from mindsdb_evaluator.accuracy.forecasting import \
     evaluate_array_accuracy, \
     evaluate_num_array_accuracy, \
@@ -8,5 +8,5 @@ from mindsdb_evaluator.accuracy.forecasting import \
     complementary_smape_array_accuracy
 
 __all__ = ['evaluate_accuracy', 'evaluate_accuracies', 'evaluate_regression_accuracy', 'evaluate_multilabel_accuracy',
-           'evaluate_array_accuracy', 'evaluate_num_array_accuracy', 'evaluate_cat_array_accuracy',
-           'complementary_smape_array_accuracy']  # noqa
+           'evaluate_top_k_accuracy', 'evaluate_array_accuracy', 'evaluate_num_array_accuracy',
+           'evaluate_cat_array_accuracy', 'complementary_smape_array_accuracy']  # noqa
--- tests/test_classification_accs.py
+++ tests/test_classification_accs.py
@@ -1,7 +1,10 @@
 import unittest
 import numpy as np
+import sys
 
-from mindsdb_evaluator.accuracy.classification import evaluate_multilabel_accuracy
+sys.path.insert(0, "../mindsdb_evaluator/accuracy")
+from classification import evaluate_multilabel_accuracy, evaluate_top_k_accuracy
+# from mindsdb_evaluator.accuracy.classification import evaluate_multilabel_accuracy, evaluate_top_k_accuracy
 
 
 class TestClassificationAccuracies(unittest.TestCase):
@@ -28,3 +31,38 @@ class TestClassificationAccuracies(unittest.TestCase):
         pred = np.array([0, 1, 1, 1, 1, 1])
         y_pred = {'prediction': pred}
         self.assertAlmostEqual(evaluate_multilabel_accuracy(y_true, y_pred), 0.9090909091, 10)
+
+    def test_evaluate_top_k_accuracy(self):
+        # vacuous case: no error
+        true = np.array([1, 1, 1, 0, 0, 0])
+        y_pred_true = {'prediction': true}
+        self.assertEqual(evaluate_top_k_accuracy(true, y_pred_true, k=1), 1.0)
+
+        # random
+        y_true = np.array([0, 1, 2, 2])
+        pred = np.array([[0.5, 0.2, 0.2],
+                        [0.3, 0.4, 0.2],
+                        [0.2, 0.4, 0.3],
+                        [0.7, 0.2, 0.1]])
+        y_pred = {'prediction': pred}
+        self.assertAlmostEqual(evaluate_top_k_accuracy(y_true, y_pred), 0.75)
+
+        # large error
+        y_true = np.array([0, 1, 2, 3])
+        pred = np.array([[0.15, 0.5, 0.3, 0.05],
+                        [0.3, 0.1, 0.3, 0.3],
+                        [0.3, 0.3, 0.1, 0.3],
+                        [0.3, 0.3, 0.3, 0.1]])
+        y_pred = {'prediction': pred}
+        self.assertAlmostEqual(evaluate_top_k_accuracy(y_true, y_pred, k=3), 0.25)
+
+        # small error
+        y_true = np.array([0, 1, 2, 3, 3])
+        pred = np.array([[0.15, 0.5, 0.3, 0.05],
+                        [0.3, 0.3, 0.1, 0.3],
+                        [0.3, 0.3, 0.3, 0.1],
+                        [0.3, 0.3, 0.3, 0.1],
+                        [0.1, 0.3, 0.3, 0.3]])
+        y_pred = {'prediction': pred}
+        self.assertAlmostEqual(evaluate_top_k_accuracy(y_true, y_pred, k=3), 0.80)
+

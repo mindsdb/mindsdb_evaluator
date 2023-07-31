28aa1e9f1c4e99cbdeb6d8b480d1d02b4579a518
Patricio Cerda Mardini <pcerdam@live.com>
2023-07-31 16:05:05 -0700

    drop support for 3.7, add support for 3.10 & 3.11

--- mindsdb_evaluator/__init__.py
+++ mindsdb_evaluator/__init__.py
@@ -1,7 +1,7 @@
 from mindsdb_evaluator.accuracy import *  # noqa
 from mindsdb_evaluator.calibration import *  # noqa
 
-__version__ = '0.0.8'
+__version__ = '0.0.10'
 name = "mindsdb_evaluator"
 
 __all__ = ['name', '__version__']

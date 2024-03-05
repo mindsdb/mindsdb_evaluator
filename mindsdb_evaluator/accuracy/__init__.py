153697a4560dd8a3dbb849cbdea86fa1be6d4a9a
Patricio Cerda Mardini <pcerdam@live.com>
2024-03-05 13:38:05 +0100

    alternative to exec

--- mindsdb_evaluator/accuracy/__init__.py
+++ mindsdb_evaluator/accuracy/__init__.py
@@ -1,3 +1,6 @@
+import importlib
+from sklearn.metrics import __all__ as __sklearn_accs__
+
 from mindsdb_evaluator.accuracy.general import evaluate_accuracy, evaluate_accuracies
 from mindsdb_evaluator.accuracy.regression import evaluate_regression_accuracy
 from mindsdb_evaluator.accuracy.classification import evaluate_multilabel_accuracy, evaluate_top_k_accuracy
@@ -7,12 +10,13 @@ from mindsdb_evaluator.accuracy.forecasting import \
     evaluate_cat_array_accuracy, \
     complementary_smape_array_accuracy
 
-from sklearn.metrics import __all__ as __sklearn_accs__
+
 __sklearn_accs__ = [acc for acc in __sklearn_accs__ if acc[0].lower() == acc[0]]
 
-# TODO: enable custom arg passing for sklearn metrics, also need to_ordinal and back, stateful?
+# TODO: enable custom arg passing for sklearn metrics, and to_ordinal (stateful?)
+sk_metrics = importlib.import_module('sklearn.metrics')
 for skacc in __sklearn_accs__:
-    exec(f'from sklearn.metrics import {skacc}')
+    globals()[skacc] = getattr(sk_metrics, skacc)
 
 __all__ = ['evaluate_accuracy', 'evaluate_accuracies', 'evaluate_regression_accuracy', 'evaluate_multilabel_accuracy',
            'evaluate_top_k_accuracy', 'evaluate_array_accuracy', 'evaluate_num_array_accuracy',

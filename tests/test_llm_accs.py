57ebc211d7b69f48b20ff86daae4fc8267f55b7a
abhayasr <abhayasravikumar@gmail.com>
2024-08-06 15:27:26 -0700

    skip test case if no key

--- tests/test_llm_accs.py
+++ tests/test_llm_accs.py
@@ -1,32 +1,31 @@
 import unittest
+import os
 
 from mindsdb_evaluator.accuracy.llm_generation import evaluate_rag
 
 
 class TestLLM(unittest.TestCase):
+    @unittest.skipIf(os.environ.get('OPENAI_API_KEY') is None, reason='Missing API key!')
     def test_evaluate_rag(self):
-        try:
-            question = ['What is the capital of France?']
-            answer = ['The capital of France is Paris']
-            contexts = ['Paris is the capital city of France, known for its art, fashion, and culture.']
-            ground_truth = ['The capital of France is Paris']
+        question = ['What is the capital of France?']
+        answer = ['The capital of France is Paris']
+        contexts = ['Paris is the capital city of France, known for its art, fashion, and culture.']
+        ground_truth = ['The capital of France is Paris']
 
-            # check faithfulness
-            self.assertEqual(evaluate_rag('faithfulness', answer, contexts, question, ground_truth), 1.0)
+        # check faithfulness
+        self.assertEqual(evaluate_rag('faithfulness', answer, contexts, question, ground_truth), 1.0)
 
-            # check answer relevancy
-            answer_relevancy = evaluate_rag('answer_relevancy', answer, contexts, question, ground_truth)
-            self.assertGreaterEqual(answer_relevancy, 0.0)
-            self.assertLessEqual(answer_relevancy, 1.01)
+        # check answer relevancy
+        answer_relevancy = evaluate_rag('answer_relevancy', answer, contexts, question, ground_truth)
+        self.assertGreaterEqual(answer_relevancy, 0.0)
+        self.assertLessEqual(answer_relevancy, 1.01)
 
-            # check context recall
-            context_recall = evaluate_rag('context_recall', answer, contexts, question, ground_truth)
-            self.assertGreaterEqual(context_recall, 0.0)
-            self.assertLessEqual(context_recall, 1.01)
+        # check context recall
+        context_recall = evaluate_rag('context_recall', answer, contexts, question, ground_truth)
+        self.assertGreaterEqual(context_recall, 0.0)
+        self.assertLessEqual(context_recall, 1.01)
 
-            # check context precision
-            context_precision = evaluate_rag('context_precision', answer, contexts, question, ground_truth)
-            self.assertGreaterEqual(context_precision, 0.0)
-            self.assertLessEqual(context_precision, 1.01)
-        except:
-            pass
+        # check context precision
+        context_precision = evaluate_rag('context_precision', answer, contexts, question, ground_truth)
+        self.assertGreaterEqual(context_precision, 0.0)
+        self.assertLessEqual(context_precision, 1.01)
